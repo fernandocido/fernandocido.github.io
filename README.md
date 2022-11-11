615b82b38bd8d82f449043d02d9981659eed302a
Fernando de Freitas <cidoguita@gmail.com>
2022-11-11 14:54:26 -0300

    First Run

---
+++ README.md
@@ -0,0 +1,5 @@
+# Personal-links
+## Simple personal website with social networks
+
+![Simple Personal Website Light Mode](/img/site-light.jpg)
+![Simple Personal Website Dark Mode](/img/site-dark.jpg)
---
+++ scripts.js
@@ -0,0 +1,34 @@
+var light = 0;
+  
+function darkMode() {
+    var element = document.body;
+    element.classList.toggle("darkMode");
+    
+    if(light==0) {
+      document.getElementById('img-email').src = "img/email-128px-dark.png";
+      document.getElementById('img-location').src = "img/location-128px-dark.png";
+      document.getElementById('img-linkedin').src = "img/linkedin-128px-dark.png";
+      document.getElementById('img-github').src = "img/github-128px-dark.png";
+      document.getElementById('img-instagram').src = "img/instagram-128px-dark.png";
+      document.getElementById('img-twitter').src = "img/twitter-128px-dark.png";
+      document.getElementById('img-lattes').src = "img/lattes-128px-dark.png";
+      document.getElementById('img-apple-music').src = "img/apple-music-128px-dark.png";
+      document.getElementById('img-lightbulb').src = "img/lightbulb-64px-dark.png";
+      document.getElementById('img-steam').src = "img/steam-64px-dark.png";
+      light = 1;
+    }
+    
+    else if(light=1) {
+      document.getElementById('img-email').src = "img/email-128px-light.png";
+      document.getElementById('img-location').src = "img/location-128px-light.png";
+      document.getElementById('img-linkedin').src = "img/linkedin-128px-light.png";
+      document.getElementById('img-github').src = "img/github-128px-light.png";
+      document.getElementById('img-instagram').src = "img/instagram-128px-light.png";
+      document.getElementById('img-twitter').src = "img/twitter-128px-light.png";
+      document.getElementById('img-lattes').src = "img/lattes-128px-light.png";
+      document.getElementById('img-apple-music').src = "img/apple-music-128px-light.png";
+      document.getElementById('img-lightbulb').src = "img/lightbulb-64px-light.png";
+      document.getElementById('img-steam').src = "img/steam-64px-light.png";
+      light = 0;
+    }
+}
