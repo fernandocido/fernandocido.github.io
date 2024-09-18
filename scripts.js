a1c687e28b9a764eb7ec521e1a7d29c8b220be13
fernandocido <cidoguita@gmail.com>
2024-09-18 02:29:32 -0300

    add insta; remove linkedin; clear js file; update screenshots.

--- scripts.js
+++ scripts.js
@@ -6,27 +6,25 @@ function darkMode() {
     
     if(light==0) {
       document.getElementById('img-lightbulb').src = "assets/img/lightbulb2.png";
-      //document.getElementById('img-location').src = "assets/img/location2.png";
-      document.getElementById('img-linkedin').src = "assets/img/linkedin2.png";
       document.getElementById('img-github').src = "assets/img/github2.png";
       document.getElementById('img-vsco').src = "assets/img/vsco2.png";
       document.getElementById('img-spotify').src = "assets/img/spotify2.png";
       document.getElementById('img-youtube').src = "assets/img/youtube2.png";
       document.getElementById('img-bmc').src = "assets/img/bmc2.png";
       document.getElementById('img-steam').src = "assets/img/steam2.png";
+      document.getElementById('img-insta').src = "assets/img/insta2.png";
       light = 1;
     }
     
     else if(light=1) {
       document.getElementById('img-lightbulb').src = "assets/img/lightbulb.png";
-      //document.getElementById('img-location').src = "assets/img/location.png";
-      document.getElementById('img-linkedin').src = "assets/img/linkedin.png";
       document.getElementById('img-github').src = "assets/img/github.png";
       document.getElementById('img-vsco').src = "assets/img/vsco.png";
       document.getElementById('img-spotify').src = "assets/img/spotify.png";
       document.getElementById('img-youtube').src = "assets/img/youtube.png";
       document.getElementById('img-bmc').src = "assets/img/bmc.png";
       document.getElementById('img-steam').src = "assets/img/steam.png";
+      document.getElementById('img-insta').src = "assets/img/insta.png";
       light = 0;
     }
 }
